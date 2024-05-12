ec7006b893ffc7359238c2300f30034281bc54d6
C. Tinkelrot <chavit@skyvar.co.il>
2024-05-12 15:46:16 +0300

    deploy

--- scripts/policy_assignments.py
+++ scripts/policy_assignments.py
@@ -3,33 +3,42 @@ from azure.mgmt.resource.policy import PolicyClient
 from azure.mgmt.resource.policy.models import PolicyAssignment, Identity
 from azure.identity import AzureCliCredential
 
+import os
+from dotenv import load_dotenv
 
-subId = "a173eef2-33d7-4d55-b0b5-18b271f8d42b"
-assignmentLocation = "East US"
+load_dotenv()
+
+
+subscription_id = os.getenv("SUBSCRIPTION_ID")
+workspace_id = os.getenv("WORKSPACE_ID")
+resource_group = os.getenv("RESOURCE_GROUP")
+assignment_location = os.getenv("ASSIGNMENT_LOCATION")
+managed_identity_name = os.getenv("MANAGED_IDENTITY")
+
 credential = AzureCliCredential()
-policyClient = PolicyClient(credential, subId, base_url="https://management.azure.com/")
+policyClient = PolicyClient(credential, subscription_id, base_url="https://management.azure.com/")
 
-policyAssignmentIdentity = Identity(type="UserAssigned",user_assigned_identities={"/subscriptions/a173eef2-33d7-4d55-b0b5-18b271f8d42b/resourcegroups/AKS/providers/Microsoft.ManagedIdentity/userAssignedIdentities/userAssigned":{}})
 
-workspace_id = "/subscriptions/a173eef2-33d7-4d55-b0b5-18b271f8d42b/resourcegroups/DefaultResourceGroup-EUS/providers/microsoft.operationalinsights/workspaces/DefaultWorkspace-a173eef2-33d7-4d55-b0b5-18b271f8d42b-EUS"
+policy_assignment_identity = Identity(type="UserAssigned",user_assigned_identities={f"/subscriptions/{subscription_id}/resourcegroups/{resource_group}/providers/Microsoft.ManagedIdentity/userAssignedIdentities/policy":{}})
+
 parameters = {
     "logAnalytics": {
         "value": workspace_id
     }
 }
 
-assignments_name=["queuepolicybycodenorg","tablepolicybycodenorg","blobpolicybycodenorg","filepolicybycodenorg"]
+assignments_name=["queuepolicy","tablepolicy","blobpolicy","filepolicy"]
 definitions_id=["7bd000e3-37c7-4928-9f31-86c4b77c5c45","2fb86bf3-d221-43d1-96d1-2434af34eaa0","7bd000e3-37c7-4928-9f31-86c4b77c5c45","25a70cc8-2bd4-47f1-90b6-1478e4662c96"]
 for i in range(0,4):
-    policyAssignmentDetails1 = PolicyAssignment(display_name=assignments_name[i],
+    policy_assignment_details = PolicyAssignment(display_name=assignments_name[i],
                                                policy_definition_id=f"/providers/Microsoft.Authorization/policyDefinitions/{definitions_id[i]}",
                                                 description="Updating log analytics",
-                                                identity=policyAssignmentIdentity,
-                                                location=assignmentLocation,
+                                                identity=policy_assignment_identity,
+                                                location=assignment_location,
                                                 parameters=parameters)
 
-    policyAssignment = policyClient.policy_assignments.create("/subscriptions/a173eef2-33d7-4d55-b0b5-18b271f8d42b",
+    policy_assignment = policyClient.policy_assignments.create(f"/subscriptions/{subscription_id}",
                                                               assignments_name[i],
-                                                              policyAssignmentDetails1)
-    print(policyAssignment)
+                                                              policy_assignment_details)
+    print(policy_assignment)
 
--- scripts/remediation_task_runbook.ps1
+++ scripts/remediation_task_runbook.ps1
@@ -1,13 +1,17 @@
-
-$subscription = "Moon- azure camp"
-$identity = "3e455f37-1e8b-4dd6-bff0-26d3859e7d37"
+param(
+[Parameter(Mandatory=$true)]
+        [string] 
+        $identity
+)
+$subscription = "MOON-NM-DIG-DEV"
+# $identity = "3e455f37-1e8b-4dd6-bff0-26d3859e7d37"
 $AzureContext = (Connect-AzAccount -Identity -AccountId $identity).context
 $connectionResult = Set-AzContext -Subscription $subscription -DefaultProfile $AzureContext
 
 $policyAssignmentIdArray="tablepolicybycodenorg","queuepolicybycodenorg","filepolicybycodenorg","blobpolicybycodenorg"
 
 for ($var = 0; $var -le 4; $var++) {
-    $policyAssignmentId="/subscriptions/a173eef2-33d7-4d55-b0b5-18b271f8d42b/providers/microsoft.authorization/policyassignments/"+$policyAssignmentIdArray[$var]
+    $policyAssignmentId="/subscriptions/a273b4fb-6a3d-4804-a047-5d293da8811d/providers/microsoft.authorization/policyassignments/"+$policyAssignmentIdArray[$var]
     try {
         $name = "remediation"+$var
         Start-AzPolicyRemediation -PolicyAssignmentId $policyAssignmentId -Name $name 
