aae1b29d9d3c316a63e9e2f1167cdec3ecb0b42d
C. Tinkelrot <chavit@skyvar.co.il>
2024-05-16 15:29:41 +0300

    deploy

--- scripts/remediation_task_runbook.ps1
+++ scripts/remediation_task_runbook.ps1
@@ -1,14 +1,14 @@
-# Param
-# (
-#   [Parameter (Mandatory= $true)]
-#   [string] $identity,
+Param
+(
+  [Parameter (Mandatory= $true)]
+  [string] $identity,
 
-#   [Parameter (Mandatory= $true)]
-#   [string] $subscription
-# )
+  [Parameter (Mandatory= $true)]
+  [string] $subscription
+)
 
-$subscription = "MOON-NM-DIG-DEV"
-$identity = "af3790d3-869e-4582-b72a-fe020311d392"
+# $subscription = "MOON-NM-DIG-DEV"
+# $identity = "af3790d3-869e-4582-b72a-fe020311d392"
 $AzureContext = (Connect-AzAccount -Identity -AccountId $identity).context
 $connectionResult = Set-AzContext -Subscription $subscription -DefaultProfile $AzureContext
 
