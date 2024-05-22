d390b3d20ad3c9308ff239ea6ddf8010ceb6a293
C. Tinkelrot <chavit@skyvar.co.il>
2024-05-22 10:35:45 +0300

    deploy

--- scripts/policy_assignments.py
+++ scripts/policy_assignments.py
@@ -6,7 +6,10 @@ from azure.identity import AzureCliCredential
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(BASEDIR, '.env'))
 
 
 subscription_id = os.getenv("SUBSCRIPTION_ID")
