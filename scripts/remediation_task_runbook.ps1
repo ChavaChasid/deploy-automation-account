fc78bef1b614b1eb198b7ac2bf857b697b9bb67f
C. Tinkelrot <chavit@skyvar.co.il>
2024-05-22 10:39:33 +0300

    deploy

--- scripts/remediation_task_runbook.ps1
+++ scripts/remediation_task_runbook.ps1
@@ -12,7 +12,7 @@ Param
 $AzureContext = (Connect-AzAccount -Identity -AccountId $identity).context
 $connectionResult = Set-AzContext -Subscription $subscription -DefaultProfile $AzureContext
 
-$policyAssignmentIdArray="queuepolicy","tablepolicy","blobpolicy","filepolicy"
+$policyAssignmentIdArray="queuepolicy2","tablepolicy2","blobpolicy2","filepolicy2"
 
 for ($var = 0; $var -le 4; $var++) {
     $policyAssignmentId="/subscriptions/a273b4fb-6a3d-4804-a047-5d293da8811d/providers/microsoft.authorization/policyassignments/"+$policyAssignmentIdArray[$var]
